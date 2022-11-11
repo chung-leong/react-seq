7e29a5f4c5c30d2c0104b801105826572d62d660
Chung Leong <chernyshevsky@hotmail.com>
2022-11-11 22:13:06 +0100

    Implemented renderToInnerHTML() (#77).

--- src/client-side.js
+++ src/client-side.js
@@ -3,11 +3,48 @@ import { renderToReadableStream } from 'react-dom/server';
 export async function renderToInnerHTML(element, node) {
   const stream = await renderToReadableStream(element);
   await stream.allReady;
-  // TODO
+  const reader = stream.getReader();
+  const chunks = [];
+  let size = 0;
+  for (;;) {
+    const { done, value } = await reader.read();
+    if (!done) {
+      size += value.length;
+      chunks.push(value);
+    } else {
+      break;
+    }
+  }
+  // merge the chunks together
+  const merged = new Uint8Array(size);
+  let pos = 0;
+  for (const chunk of chunks) {
+    merged.set(chunk, pos);
+    pos += chunk.length;
+  }
+  // convert to string
+  const converter = new TextDecoder();
+  const html = converter.decode(merged);
+  node.innerHTML = html;
 }
 
 export async function renderToServer(element) {
   const stream = await renderToReadableStream(element);
   await stream.allReady;
   global?.send(stream);
+}
+
+export function hasSuspended(root) {
+  function check(node) {
+    if (node.memoizedState && 'dehydrated' in node.memoizedState) {
+      return true;
+    }
+    for (let c = node.child; c; c = c.sibling) {
+      if (check(c)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  return check(root._internalRoot.current);
 }
