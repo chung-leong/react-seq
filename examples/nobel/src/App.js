87cbe9d5bb42d2e56f3499004841ba48a886f9c4
Chung Leong <chernyshevsky@hotmail.com>
2022-11-23 02:14:20 +0100

    Added Nobel prize example (#87).

---
+++ examples/nobel/src/App.js
@@ -0,0 +1,75 @@
+import './css/App.css';
+import { useState } from 'react';
+import { useNobelPrize } from './nobel.js';
+
+const categories = [
+  { code: 'che', name: 'Chemistry' },
+  { code: 'eco', name: 'Economics' },
+  { code: 'lit', name: 'Literature' },
+  { code: 'pea', name: 'Peace' },
+  { code: 'phy', name: 'Physics' },
+  { code: 'med', name: 'Medicines' },
+];
+const currentYear = (new Date()).getYear() + 1900;
+const years = [ ...(function*() {
+  for (let y = 1901; y < currentYear; y++) {
+    yield `${y}`;
+  }
+})() ];
+
+function App() {
+  const [ year, setYear ] = useState('');
+  const [ category, setCategory ] = useState('');
+  const prize = useNobelPrize(category, year);
+  return (
+    <div className="App">
+      <div className="toolbar">
+        <select onChange={evt => setCategory(evt.target.value)} value={category} required>
+          <option value="">Category</option>
+          {categories.map(({ code, name }) => <option key={code} value={code}>{name}</option>)}
+        </select>
+        {' '}
+        <select onChange={evt => setYear(evt.target.value)} value={year} required>
+          <option value="">Year</option>
+          {years.map(year => <option key={year} value={year}>{year}</option>)}
+        </select>
+      </div>
+      {prize && <Prize {...prize} />}
+    </div>
+  );
+}
+
+function Prize({ fullName, amount, amountAdjusted, laureates }) {
+  return (
+    <div className="content">
+      <h2>{fullName}</h2>
+      <div><b>Amount:</b> ${amount}</div>
+      <div><b>Amount (in present dollar):</b> ${amountAdjusted}</div>
+      <h2>Laureate{laureates.length > 1 ? 's' : ''}</h2>
+      {laureates.map((laureate, i) => {
+        const {
+          fullName,
+          motivation,
+          gender,
+          birth,
+          death,
+          affiliation,
+          wikipedia,
+        } = laureate;
+        return (
+          <div key={i}>
+            <h3>{fullName}</h3>
+            <div><b>Motivation:</b> {motivation}</div>
+            <div><b>Affiliation:</b> {affiliation}</div>
+            <div><b>Gender:</b> {gender}</div>
+            <div><b>Birth:</b> {birth}</div>
+            <div><b>Death:</b> {death}</div>
+            <div><b>Wikipedia:</b> <a href={wikipedia} target="_blank">{wikipedia}</a></div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ examples/nobel/src/nobel.js
@@ -0,0 +1,78 @@
+import { useSequentialState } from 'react-seq';
+
+export function useNobelPrize(category, year) {
+  return useSequentialState(async function*({ signal }) {
+    if (!category || !year) {
+      return;
+    }
+    const prizeURL = new URL(`https://api.nobelprize.org/2.1/nobelPrize/${category}/${year}`);
+    const [ {
+      categoryFullName,
+      prizeAmount,
+      prizeAmountAdjusted,
+      laureates
+    } ] = await fetchJSON(prizeURL, { signal });
+    const prize = {
+      fullName: str(categoryFullName),
+      amount: prizeAmount,
+      amountAdjusted: prizeAmountAdjusted,
+      laureates: laureates.map(({ fullName, orgName, motivation }) => {
+        return {
+          fullName: str(fullName) ?? str(orgName),
+          motivation: str(motivation),
+        };
+      }),
+    };
+    yield prize;
+    for (const [ index, laureate ] of laureates.entries()) {
+      const [ {
+        gender,
+        birth,
+        death,
+        nobelPrizes,
+        wikipedia,
+      } ] = await fetchJSON(laureate.links.href);
+      const target = prize.laureates[index];
+      target.gender = gender;
+      target.birth = formatDatePlace(birth);
+      target.death = formatDatePlace(death);
+      // find the relevant prize
+      const matchingPrize = nobelPrizes.find(p => p.awardYear === year);
+      target.affiliation = formatAffiliation(matchingPrize?.affiliations?.[0]);
+      target.wikipedia = wikipedia.english;
+      yield prize;
+    }
+  }, [ category, year ]);
+}
+
+function str(obj) {
+  if (!obj) {
+    return;
+  }
+  return obj.en ?? obj.se ?? obj.no;
+}
+
+function formatDatePlace(obj) {
+  if (!obj) {
+    return;
+  }
+  const { date, place } = obj;
+  const { city, country } = place;
+  return `${date} (${str(city)}, ${str(country)})`;
+}
+
+function formatAffiliation(obj) {
+  if (!obj) {
+    return;
+  }
+  const { name, city, country } = obj;
+  return `${str(name)} (${str(city)}, ${str(country)})`;
+}
+
+async function fetchJSON(url, options) {
+  const res = await fetch(url, options);
+  if (res.status !== 200) {
+    throw new Error(`${res.status} ${res.statusText}`);
+  }
+  return res.json();
+}
