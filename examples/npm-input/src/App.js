92ef9220610f824a706a91b44706ea13d6c97fb8
Chung Leong <chernyshevsky@hotmail.com>
2022-12-01 03:11:23 +0100

    Added npm example.

---
+++ examples/npm-input/src/App.js
@@ -0,0 +1,98 @@
+import { useId, useDeferredValue, useState, useEffect, useCallback, startTransition } from 'react';
+import { useSequentialState, useProgressiveState } from 'react-seq';
+import './css/App.css';
+
+export default function App() {
+  const [ search1, setSearch1 ] = useState('');
+  const [ search2, setSearch2 ] = useState('');
+  const [ search3, setSearch3 ] = useState('');
+  const onChange1 = useCallback(evt => setSearch1(evt.target.value), []);
+  const onChange2 = useCallback(evt => setSearch2(evt.target.value), []);
+  const onChange3 = useCallback(evt => setSearch3(evt.target.value), []);
+  const dataList1 = useNPMList1(search1);
+  const dataList2 = useNPMList2(search2);
+  const dataList3 = useNPMList3(search3);
+  return (
+    <div className="App">
+      <div>
+        <input list={dataList1.props.id} value={search1} onChange={onChange1} />
+        {dataList1}
+      </div>
+      <div>
+        <input list={dataList2.props.id} value={search2} onChange={onChange2} />
+        {dataList2}
+      </div>
+      <div>
+        <input list={dataList3.props.id} value={search3} onChange={onChange3} />
+        {dataList3}
+      </div>
+    </div>
+  );
+}
+
+function useNPMList1(search) {
+  const id = useId();
+  const searchDeferred = useDeferredValue(search.trim());
+  const [ list, setList ] = useState([]);
+  useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+    fetchPackages(searchDeferred, { signal }).then((list) => {
+      if (!signal.aborted) {
+        startTransition(() => setList(list));
+      }
+    });
+    return () => {
+      controller.abort();
+      setList([]);
+    };
+  }, [ searchDeferred ]);
+  return createList(id, list);
+}
+
+function useNPMList2(search) {
+  const id = useId();
+  const searchDeferred = useDeferredValue(search.trim());
+  const list = useSequentialState(async function*({ initial, signal }) {
+    initial([]);
+    yield fetchPackages(searchDeferred, { signal });
+  }, [ searchDeferred ]);
+  return createList(id, list);
+}
+
+function useNPMList3(search) {
+  const id = useId();
+  const searchDeferred = useDeferredValue(search.trim());
+  const { list } = useProgressiveState(async ({ initial, signal }) => {
+    initial({ list: [] });
+    return { list: fetchPackages(searchDeferred, { signal }) };
+  }, [ searchDeferred ]);
+  return createList(id, list);
+}
+
+function createList(id, items) {
+  return (
+    <datalist id={id}>
+      {items.map((v, i) => <option key={i} value={v} />)}
+    </datalist>
+  );
+}
+
+async function fetchPackages(text, options) {
+  if (text) {
+    try {
+      const url = new URL('https://registry.npmjs.com/-/v1/search');
+      url.searchParams.set('text', text);
+      const res = await fetch(url, options);
+      if (res.status === 200) {
+        const json = await res.json();
+        return json.objects.map(r => r.package.name);
+      }
+    } catch (err) {
+      if (err.name !== 'AbortError') {
+        console.error(err);
+      }
+    }
+  }
+  return [];
+}
