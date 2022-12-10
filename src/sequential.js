6639d597ba3f7bfcc6504ee3621c8cd947be63e6
Chung Leong <chernyshevsky@hotmail.com>
2022-12-10 01:23:24 +0100

    Update sequential.js

--- src/sequential.js
+++ src/sequential.js
@@ -3,7 +3,7 @@ import { IntermittentIterator, Timeout, Interruption } from './iterator.js';
 import { EventManager } from './event-manager.js';
 import { AbortManager } from './abort-manager.js';
 import { InspectorContext } from './inspector.js';
-import { Abort, nextTick, isAbortError } from './utils.js';
+import { Abort, isAbortError } from './utils.js';
 import { setting } from './settings.js';
 
 export function useSequential(cb, deps) {
