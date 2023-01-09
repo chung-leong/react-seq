d2df9eef5ec748bdd8f60e0859f09874f32a2327
Chung Leong <chernyshevsky@hotmail.com>
2023-01-09 15:57:14 +0100

    Added explosion effect to transition example.

---
+++ examples/transition/src/Explosion.js
@@ -0,0 +1,56 @@
+import { Fragment, Children } from 'react';
+import { createPortal } from 'react-dom';
+import './css/Explosion.css';
+
+export class Explosion {
+  constructor(methods) {
+    this.methods = methods;
+    this.previous = null;
+    this.container = null;
+  }
+
+  to = (async function *(element) {
+    const { previous } = this;
+    this.previous = element;
+    if (previous && !isSameType(previous, element)) {
+      if (!this.container) {
+        this.container = document.createElement('DIV');
+        this.container.className = 'Explosion';
+        document.body.appendChild(this.container);
+      }
+      const { manageEvents } = this.methods;
+      const [ on, eventual ] = manageEvents();
+      const video = <video src="/explosion.webm" autoPlay onPlay={on.videoStart} onEnded={on.videoEnd} />;
+      const explosion = createPortal(video, this.container);
+      yield <Fragment>{previous}{explosion}</Fragment>;
+      await eventual.videoStart;
+      await eventual.videoMidpoint.for(1.1).seconds;
+      yield <Fragment>{element}{explosion}</Fragment>;
+      await eventual.videoEnd;
+    }
+    yield <Fragment>{element}</Fragment>;
+  }).bind(this);
+
+  prevent = () => {
+    this.previous = null;
+  }
+}
+
+function isSameType(el1, el2) {
+  if (el1?.type === el2?.type) {
+    if (el1?.type === Fragment) {
+      const c1 = Children.toArray(el1.props.children);
+      const c2 = Children.toArray(el2.props.children);
+      if (c1.length !== c2.length) {
+        return false;
+      }
+      for (let i = 0; i < c1.length; i++) {
+        if (!isSameType(c1[i], c2[i])) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  return false;
+}
